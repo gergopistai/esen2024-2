6ca1d413d67232f010a7110a1817f8d7af576a2c
gergopistai <gergopistai@gmail.com>
2024-04-23 09:31:27 +0200

    Add delete book and update book methods

--- src/main/java/com/esen/bookstore/shell/BookHandler.java
+++ src/main/java/com/esen/bookstore/shell/BookHandler.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 import java.util.stream.Collectors;
 
@@ -32,4 +33,19 @@ public class BookHandler {
                 .map(book -> "ID: %s\t Publisher: %s\t Author: %s\t Title: %s\t Price: %f Ft".formatted(
                         book.getId(), book.getPublisher(), book.getAuthor(), book.getTitle(), book.getPrice())).collect(Collectors.joining(System.lineSeparator()));
     }
+
+    @ShellMethod(value = "Delete books", key = "delete book")
+    public void deleteBook(Long id) {
+        bookService.deleteBook(id);
+    }
+
+    @ShellMethod(value = "Update a book", key = "update book")
+    public void updateBook(Long id,
+                           @ShellOption(defaultValue = ShellOption.NULL) String title,
+                           @ShellOption(defaultValue = ShellOption.NULL) String author,
+                           @ShellOption(defaultValue = ShellOption.NULL) String publisher,
+                           @ShellOption(defaultValue = ShellOption.NULL) Double price) {
+        bookService.updateBook(id, title, author, publisher, price);
+    }
+
 }
