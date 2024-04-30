bca19cd3944f9166935dfac9af4fe4afb794e753
gergopistai <gergopistai@gmail.com>
2024-04-30 09:30:55 +0200

    Add delete findPrices, getStock and addStock methods

--- src/main/java/com/esen/bookstore/service/BookstoreService.java
+++ src/main/java/com/esen/bookstore/service/BookstoreService.java
@@ -2,18 +2,24 @@ package com.esen.bookstore.service;
 
 import com.esen.bookstore.model.Book;
 import com.esen.bookstore.model.Bookstore;
+import com.esen.bookstore.repository.BookRepository;
 import com.esen.bookstore.repository.BookstoreRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class BookstoreService {
 
     private final BookstoreRepository bookstoreRepository;
+    private final BookRepository bookRepository;
 
     @Transactional
     public void removeBookFromInventories(Book book) {
@@ -39,5 +45,67 @@ public class BookstoreService {
     public void deleteBookstore(Long id) {
         var bookstore = bookstoreRepository.findById(id).orElseThrow(() -> new RuntimeException("Cannot find bookstore"));
         bookstoreRepository.delete(bookstore);
+    }
+
+    public void updateBookstore(Long id, String location, Double priceModifier, Double moneyInCashRegister) {
+        if (Stream.of(location, priceModifier, moneyInCashRegister).allMatch(Objects::isNull)) {
+            throw new UnsupportedOperationException("There's nothing to update");
+        }
+
+        var bookstore = bookstoreRepository.findById(id).orElseThrow(() -> new RuntimeException("Cannot find bookstore"));
+
+        if (location != null) {
+            bookstore.setLocation(location);
+        }
+
+        if (priceModifier != null) {
+            bookstore.setPriceModifier(priceModifier);
+        }
+
+        if (moneyInCashRegister != null) {
+            bookstore.setMoneyInCashRegister(moneyInCashRegister);
+        }
+
+        bookstoreRepository.save(bookstore);
+    }
+
+    public Map<Bookstore, Double> findPrices(Long id) {
+        var book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Cannot find book"));
+        var bookstores = findAll();
+
+        Map<Bookstore, Double> priceMap = new HashMap<>();
+        for (var b:bookstores) {
+            if (b.getInventory().containsKey(book)) {
+                Double currPrice = book.getPrice() * b.getPriceModifier();
+                priceMap.put(b, currPrice);
+            }
+        }
+
+        return priceMap;
+    }
+
+    public Map<Book, Integer> getStock(Long id) {
+        return bookstoreRepository.findById(id).orElseThrow(() -> new RuntimeException("Cannot find bookstore")).getInventory();
+    }
+
+    public void changeStock(Long bookstoreId, Long bookId, int amount) {
+        var bookstore = bookstoreRepository.findById(bookstoreId).orElseThrow(() -> new RuntimeException("Cannot find bookstore"));
+        var book = bookRepository.findById(bookId).orElseThrow(() -> new RuntimeException("Cannot find book"));
+
+        if (bookstore.getInventory().containsKey(book)) {
+            var entry = bookstore.getInventory().get(book);
+            if (entry + amount < 0) {
+                throw new UnsupportedOperationException("Invalid amount");
+            }
+            bookstore.getInventory().replace(book, entry + amount);
+        } else {
+            if (amount < 0) {
+                throw new UnsupportedOperationException("Invalid amount");
+            }
+            bookstore.getInventory().put(book, amount);
+        }
+
+        bookstoreRepository.save(bookstore);
+
     }
 }
--- src/main/java/com/esen/bookstore/shell/BookstoreHandler.java
+++ src/main/java/com/esen/bookstore/shell/BookstoreHandler.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 import java.util.stream.Collectors;
 
@@ -34,5 +35,43 @@ public class BookstoreHandler {
     @ShellMethod(key = "delete bookstore", value = "delete bookstore")
     public void deleteBookstore(Long id) {
         bookstoreService.deleteBookstore(id);
+    }
+
+    @ShellMethod(value = "Update a bookstore", key = "update bookstore")
+    public void updateBook(Long id,
+                           @ShellOption(defaultValue = ShellOption.NULL) String location,
+                           @ShellOption(defaultValue = ShellOption.NULL) Double priceModifier,
+                           @ShellOption(defaultValue = ShellOption.NULL) Double moneyInCashRegister) {
+        bookstoreService.updateBookstore(id, location, priceModifier, moneyInCashRegister);
+    }
+
+    @ShellMethod(value = "Find prices", key="find prices")
+    public String findPrices(Long bookId) {
+        return bookstoreService.findPrices(bookId)
+                .entrySet()
+                .stream()
+                .map(entry -> "Bookstore ID: %d, Location: %s, Price: %f Ft".formatted(
+                        entry.getKey().getId(),
+                        entry.getKey().getLocation(),
+                        entry.getValue()))
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    @ShellMethod(value = "Get stock", key="get stock")
+    public String getStock(Long id) {
+        return bookstoreService.getStock(id)
+                .entrySet()
+                .stream()
+                .map(entry -> "%d. %s - %s\t| Copies: %d".formatted(
+                        entry.getKey().getId(),
+                        entry.getKey().getAuthor(),
+                        entry.getKey().getTitle(),
+                        entry.getValue()))
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    @ShellMethod(value = "Add stock", key = "add stock")
+    public void addStock(Long bookstoreId, Long bookId, int amount) {
+        bookstoreService.changeStock(bookstoreId, bookId, amount);
     }
 }
